eca15f76521b0a26ab3b2e1252948702a69d1d6f
alamin <alamintechlyn>
2024-11-15 15:26:20 +0600

    add faq section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
-  // new arrival section 
-  document.addEventListener("DOMContentLoaded", function() {
-    var cards = document.getElementsByClassName("pf-new-arrival-card");
-    cards[0]?.classList.add("active");
-    for (var i = 0; i < cards.length; i++) {
+  // faq js  
+  document.querySelectorAll('.pf-top').forEach( function(top){
+    top.addEventListener('click', function(){
+        const content = this.nextElementSibling;
+        console.log(content)
+        const isOpen = content.style.maxHeight &&  content.style.maxHeight !== '0px';
+        if(isOpen){
+            content.style.maxHeight = '0';
+            content.style.padding = '0 10px';
+            this.querySelector('svg').style.transform = 'rotate(0deg)'; 
+        }
+        else{
+            content.style.maxHeight = content.scrollHeight + 'px';
+            content.style.padding = '10px';
+            this.querySelector('svg').style.transform = 'rotate(180deg)';
+        }
+       // remove all accordian when click btn one bye one open
+       document.querySelectorAll('.pf-bottom').forEach(function(content){
+        if(content !== top.nextElementSibling){
+            content.style.maxHeight = '0';
+            content.style.padding = '0 10px';
+            content.previousElementSibling.querySelector('svg').style.transform = 'rotate(0deg)';
+        }
+    })
 
-        (function(index) {
-            cards[index].addEventListener("mouseover", function() {
-                // Remove active class from all cards
-                for (var j = 0; j < cards.length; j++) {
-                    cards[j].classList.remove("active");
-                }
-                // Add active class to the hovered card
-                cards[index].classList.add("active");
-            });
-        })(i);
-    }
-});
-
-
+    })
+})
